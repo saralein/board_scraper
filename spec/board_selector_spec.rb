a3083b6067549076623fc86737f977693eac7b99
saralein <sara.m.trujillo@gmail.com>
2017-08-09 16:12:51 -0500

    [Refactor] Update selector tests to use before each block for instance variable and method mocking.

--- spec/board_selector_spec.rb
+++ spec/board_selector_spec.rb
@@ -6,6 +6,14 @@ describe BoardSelector do
   short_crafters = ['Fred', 'George']
   requested_number = 3
 
+  before(:each) do
+    allow_any_instance_of(BoardScraper).to receive(:get_crafters).and_return(crafters)
+    @selector = BoardSelector.new(requested_number)
+
+    allow_any_instance_of(BoardScraper).to receive(:get_crafters).and_return(short_crafters)
+    @short_selector = BoardSelector.new(requested_number)
+  end
+
   describe 'generate_random_numbers' do
     selector = BoardSelector.new(requested_number)
     numbers = selector.generate_random_numbers
@@ -21,48 +29,37 @@ describe BoardSelector do
 
   describe 'crafters_list_is_long_enough' do
     it 'returns false if list is too short' do
-      allow_any_instance_of(BoardScraper).to receive(:get_crafters).and_return(short_crafters)
-      short_selector = BoardSelector.new(requested_number)
-      expect(short_selector.crafters_list_is_long_enough).to eql(false)
+      expect(@short_selector.crafters_list_is_long_enough).to eql(false)
     end
 
     it 'returns true if list is long enough' do
-      allow_any_instance_of(BoardScraper).to receive(:get_crafters).and_return(crafters)
-      selector = BoardSelector.new(requested_number)
-      expect(selector.crafters_list_is_long_enough).to eql(true)
+      expect(@selector.crafters_list_is_long_enough).to eql(true)
     end
   end
 
   describe 'select_crafters' do
     it 'returns an array of three crafters' do
-      allow_any_instance_of(BoardScraper).to receive(:get_crafters).and_return(crafters)
-      selector = BoardSelector.new(requested_number)
-      allow(selector).to receive(:generate_random_numbers).and_return([1, 2, 3])
-      expect(selector.select_crafters).to eql(['Paul', 'Mary', 'Bill'])
+      allow(@selector).to receive(:generate_random_numbers).and_return([1, 2, 3])
+      expect(@selector.select_crafters).to eql(['Paul', 'Mary', 'Bill'])
     end
 
     it 'returns an array of three different crafters' do
-      allow_any_instance_of(BoardScraper).to receive(:get_crafters).and_return(crafters)
-      selector = BoardSelector.new(requested_number)
-      allow(selector).to receive(:generate_random_numbers).and_return([0, 3, 4])
-      expect(selector.select_crafters).to eql(['Peter', 'Bill', 'Nancy'])
+      allow(@selector).to receive(:generate_random_numbers).and_return([0, 3, 4])
+      expect(@selector.select_crafters).to eql(['Peter', 'Bill', 'Nancy'])
     end
 
     it 'return original crafters list if list is too short' do
-      allow_any_instance_of(BoardScraper).to receive(:get_crafters).and_return(short_crafters)
-      short_selector = BoardSelector.new(requested_number)
-      expect(short_selector.select_crafters).to eql(short_crafters)
+      expect(@short_selector.select_crafters).to eql(short_crafters)
     end
   end
 
   describe 'display_crafters' do
     it 'calls puts requested_number times' do
-      selector = BoardSelector.new(requested_number)
-      allow(selector).to receive(:select_crafters).and_return(['Peter', 'Bill', 'Nancy'])
+      allow(@selector).to receive(:select_crafters).and_return(['Peter', 'Bill', 'Nancy'])
       expect(STDOUT).to receive(:puts).with('Peter').ordered
       expect(STDOUT).to receive(:puts).with('Bill').ordered
       expect(STDOUT).to receive(:puts).with('Nancy').ordered
-      selector.display_crafters
+      @selector.display_crafters
     end
   end
 end
