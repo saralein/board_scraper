5e72b077f8936d0b8714ea298332faa2bef8692a
saralein <sara.m.trujillo@gmail.com>
2017-08-10 08:22:28 -0500

    [Refactor] Change app to use more descriptive function for picking review board.

--- app.rb
+++ app.rb
@@ -1,5 +1,12 @@
 require 'bundler/setup'
+require './lib/board_scraper.rb'
 require './lib/board_selector.rb'
 Bundler.require
 
-BoardSelector.new(6).display_crafters
+def pick_review_board(number_of_crafters)
+  BoardSelector
+    .new(number_of_crafters, BoardScraper.new.get_crafters)
+    .display_crafters
+end
+
+pick_review_board(6)
